fe574308a337dbdf5fd4b629fad82468bfeb888d
Porto-Yospunya <yospunyaporto@gmail.com>
2024-09-02 23:13:44 +0700

    up[date

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const Member = require('./../models/member.model');
+const Admin = require('./../models/admin.model');
 
 const validateToken = async (req, res, next) => {
     const token = req.cookies.token;
@@ -23,6 +24,26 @@ const validateToken = async (req, res, next) => {
     }
 }
 
+const validateAdminToken = async (req, res, next) => {
+    const token = req.cookies.token2;
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) {
+                res.redirect('/');
+                console.log("Admin is not authosized.");
+            }
+            req.name = decoded.name;
+
+            next();
+        });
+    } else {
+        res.redirect('/');
+        console.log("Admin is not authosized.");
+    }
+}
+
+
 const checkMember = (req, res, next) => {
     const token = req.cookies.token;
     res.locals.member = null;
@@ -40,4 +61,21 @@ const checkMember = (req, res, next) => {
     }
 }
 
-module.exports = { validateToken, checkMember };
+const checkAdmin = (req, res, next) => {
+    const token = req.cookies.token2;
+    res.locals.admin = null;
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+            if (err) { next(); };
+
+            const admin = await Admin.findById(decoded.id);
+            res.locals.admin = admin;
+            next();
+        });
+    } else {
+        next();
+    }
+}
+
+module.exports = { validateToken, validateAdminToken, checkMember, checkAdmin };
---
+++ src/models/admin.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
+const adminsSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: false,
+        default: "admin",
+    }
+}, { timestamps: true });
+
+adminsSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+module.exports = mongoose.model("Admin", adminsSchema);
